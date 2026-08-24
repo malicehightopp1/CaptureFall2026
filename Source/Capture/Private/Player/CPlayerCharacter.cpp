c550810bcaad3a77599e952686a013b769c92d38
brandin stanfield <brandin.stanfield@gmail.com>
2026-08-24 17:36:17 -0500

    Jump mechanic added and basic architecture

--- Source/Capture/Private/Player/CPlayerCharacter.cpp
+++ Source/Capture/Private/Player/CPlayerCharacter.cpp
@@ -3,3 +3,41 @@
 
 #include "Player/CPlayerCharacter.h"
 
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
+
+ACPlayerCharacter::ACPlayerCharacter()
+{
+	//Player spring arm
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>("Camera Boom");
+	CameraBoom->SetupAttachment(GetRootComponent());
+	
+	//Player camera
+	ViewCam = CreateDefaultSubobject<UCameraComponent>("View Cam");
+	ViewCam->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+}
+
+void ACPlayerCharacter::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+	
+	UEnhancedInputLocalPlayerSubsystem* EnhancedInputLocalPlayerSubsystem = GetController<APlayerController>()->GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>();
+	
+	if (EnhancedInputLocalPlayerSubsystem)
+	{
+		EnhancedInputLocalPlayerSubsystem->ClearAllMappings();
+		EnhancedInputLocalPlayerSubsystem->AddMappingContext(PlayerInputMappingContext, 0);
+	}
+}
+
+void ACPlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(JumpingAction, ETriggerEvent::Triggered, this, &ACPlayerCharacter::Jump);
+	}
+}
--- Source/Capture/Private/Player/CPlayerCharacter.h
+++ Source/Capture/Private/Player/CPlayerCharacter.h
@@ -6,10 +6,28 @@
 #include "Characters/CCharacter.h"
 #include "CPlayerCharacter.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
 
 UCLASS()
 class ACPlayerCharacter : public ACCharacter
 {
 	GENERATED_BODY()
+	
+public:
+	ACPlayerCharacter();
+	
+	virtual void PawnClientRestart() override; //called everytime the client reconnects or connects, aka if you join the game it starts your stuff then
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+private:
+	
+	//Camera setup
+	UPROPERTY(VisibleDefaultsOnly, Category = "Camera") class USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleDefaultsOnly, Category = "Camera") class UCameraComponent* ViewCam;
+	
+	//Inputs
+	UPROPERTY(EditDefaultsOnly, Category = "Input") class UInputMappingContext* PlayerInputMappingContext;
+	UPROPERTY(EditDefaultsOnly, Category = "Input") class UInputAction* JumpingAction;
 	
 };
